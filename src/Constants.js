61a69e813de9dae32da9680ba29ec92266e194f0
parkourkarthik <parkourkarthik@gmail.com>
2020-10-12 22:56:35 +0530

    Refactor social connects with components Fixes #8

---
+++ src/Constants.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    name: 'EMPLOYEE REVIEW',
+    url: 'https://github.com/ParkourKarthik/EmployeeReview',
+    techs: 'Reactjs, nodejs, Mongodb',
+    imageUrl:
+      'https://user-images.githubusercontent.com/9585305/86875085-54caed00-c0ff-11ea-9045-2a092a28441c.png'
+  },
+  {
+    name: 'SIMPLE SCHEDULER',
+    url: 'https://github.com/ParkourKarthik/simplescheduler',
+    mobile: true,
+    techs: 'ReactNative, nodejs, Mongodb',
+    imageUrl:
+      'https://user-images.githubusercontent.com/9585305/85940522-8b07b000-b93a-11ea-8f0f-6b0bf9ce76f8.png'
+  },
+  {
+    name: 'COLOR CONFUSION',
+    url: 'https://github.com/ParkourKarthik/ColorConfusion',
+    mobile: true,
+    techs: 'Kotlin',
+    imageUrl:
+      'https://user-images.githubusercontent.com/9585305/86947730-ce97c080-c169-11ea-967b-a603069adfaa.png'
+  },
+  {
+    name: 'SHIP GAME',
+    url: 'https://github.com/ParkourKarthik/ship-game',
+    techs: 'HTML, JS, CSS',
+    imageUrl:
+      'https://user-images.githubusercontent.com/9585305/86877760-1cc6a880-c105-11ea-8dcb-29e1d7bb9e65.png'
+  }
+];
+
+const socialConnects = [
+  {
+    name: "GitHub",
+    link: "https://github.com/parkourkarthik",
+    icon: "fa-github"
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/Parkourkarthik",
+    icon: "fa-twitter"
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/karthikayan-pazhanivel-501a0051",
+    icon: "fa-linkedin"
+  },
+  {
+    name: "StackOverflow",
+    link: "https://stackoverflow.com/users/3098008/parkourkarthik",
+    icon: "fa-stack-overflow"
+  }
+];
+
+export {projects, socialConnects};
