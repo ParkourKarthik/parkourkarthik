bd3fd12d44df9775d5f066a7ba64b83546306e06
karthik-baton <karthikayan.pazhanivel@batonsystems.com>
2021-02-20 11:22:56 +0530

    fix #19 - add waiter project

--- src/Constants.js
+++ src/Constants.js
@@ -2,17 +2,25 @@ const projects = [
   {
     name: 'EMPLOYEE REVIEW',
     url: 'https://github.com/ParkourKarthik/EmployeeReview',
-    techs: 'Reactjs, nodejs, Mongodb',
+    techs: 'Reactjs, Nodejs, Mongodb',
     imageUrl:
-      'https://user-images.githubusercontent.com/9585305/86875085-54caed00-c0ff-11ea-9045-2a092a28441c.png'
+      'https://user-images.githubusercontent.com/9585305/86875085-54caed00-c0ff-11ea-9045-2a092a28441c.png',
+  },
+  {
+    name: 'THE WAITER',
+    url: '',
+    mobile: true,
+    techs: 'Reactjs',
+    imageUrl:
+      'https://dev-to-uploads.s3.amazonaws.com/i/9k2t7lpofzvlsgwpn594.png',
   },
   {
     name: 'SIMPLE SCHEDULER',
     url: 'https://github.com/ParkourKarthik/simplescheduler',
     mobile: true,
-    techs: 'ReactNative, nodejs, Mongodb',
+    techs: 'ReactNative, Nodejs, Mongodb',
     imageUrl:
-      'https://user-images.githubusercontent.com/9585305/97676350-87d22b80-1ab6-11eb-8baa-5560d336f913.jpg'
+      'https://user-images.githubusercontent.com/9585305/97676350-87d22b80-1ab6-11eb-8baa-5560d336f913.jpg',
   },
   {
     name: 'COLOR CONFUSION',
@@ -20,38 +28,38 @@ const projects = [
     mobile: true,
     techs: 'Kotlin',
     imageUrl:
-      'https://user-images.githubusercontent.com/9585305/97676511-c7007c80-1ab6-11eb-9754-828f5a2cf07f.jpg'
+      'https://user-images.githubusercontent.com/9585305/97676511-c7007c80-1ab6-11eb-9754-828f5a2cf07f.jpg',
   },
   {
     name: 'SHIP GAME',
     url: 'https://github.com/ParkourKarthik/ship-game',
     techs: 'HTML, JS, CSS',
     imageUrl:
-      'https://user-images.githubusercontent.com/9585305/86877760-1cc6a880-c105-11ea-8dcb-29e1d7bb9e65.png'
-  }
+      'https://user-images.githubusercontent.com/9585305/86877760-1cc6a880-c105-11ea-8dcb-29e1d7bb9e65.png',
+  },
 ];
 
 const socialConnects = [
   {
-    name: "GitHub",
-    link: "https://github.com/parkourkarthik",
-    icon: "fa-github"
+    name: 'GitHub',
+    link: 'https://github.com/parkourkarthik',
+    icon: 'fa-github',
   },
   {
-    name: "Twitter",
-    link: "https://twitter.com/Parkourkarthik",
-    icon: "fa-twitter"
+    name: 'Twitter',
+    link: 'https://twitter.com/Parkourkarthik',
+    icon: 'fa-twitter',
   },
   {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/karthikayan-pazhanivel-501a0051",
-    icon: "fa-linkedin"
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/karthikayan-pazhanivel-501a0051',
+    icon: 'fa-linkedin',
   },
   {
-    name: "StackOverflow",
-    link: "https://stackoverflow.com/users/3098008/parkourkarthik",
-    icon: "fa-stack-overflow"
-  }
+    name: 'StackOverflow',
+    link: 'https://stackoverflow.com/users/3098008/parkourkarthik',
+    icon: 'fa-stack-overflow',
+  },
 ];
 
-export {projects, socialConnects};
+export { projects, socialConnects };
